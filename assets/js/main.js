60de621440e8706aeda46bf049d968e6508ae16d
Rodolpho Becker <becker.rodolpho@gmail.com>
2022-01-02 18:27:53 -0300

    structural changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -104,23 +104,6 @@ let swiperPortfolio = new Swiper('.portfolio__container', {
   }
 })
 
-/* ==================== TESTIMONIAL ==================== */
-let swiperTestimonial = new Swiper('.testimonial__container', {
-  loop: true,
-  grabCursor: true,
-  spaceBetween: 48,
-
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-    dynamicBullets: true
-  },
-  breakpoints: {
-    568: {
-      slidesPerView: 2
-    }
-  }
-})
 /* ==================== SCROLL SECTIONS ACTIVE LINK ==================== */
 const sections = document.querySelectorAll('section[id]')
 
